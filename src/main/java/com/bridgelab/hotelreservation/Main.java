7564cbce6a781797ffae1634b9ceb2f26e5cade3
ShraddhaJ2802 <122585974+ShraddhaJ2802@users.noreply.github.com>
2024-05-17 12:55:48 +0530

    Update Main.java

--- src/main/java/com/bridgelab/hotelreservation/Main.java
+++ src/main/java/com/bridgelab/hotelreservation/Main.java
@@ -2,6 +2,7 @@ package com.bridgelab.hotelreservation;
 
 public class Main {
     public static void main(String[] args) {
+        //Welcome Msg
         System.out.println("Welcome to hotel reservation");
     }
 }
