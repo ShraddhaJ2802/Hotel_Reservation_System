257a270b8f46f9fa87589b3d9d29869d469cf643
Shraddha <jadhavshradddha5@gmail.com>
2024-05-17 12:54:29 +0530

    Ability to add ratings to each Hotel

--- src/main/java/com/bridgelab/hotelreservation/Hotel.java
+++ src/main/java/com/bridgelab/hotelreservation/Hotel.java
@@ -7,6 +7,8 @@ public class Hotel {
     private double weekdayRate;
     private double weekendRate;
 
+    private int rating;
+
 
     public Hotel(String name, double regularRate) {
         this.name = name;
@@ -22,6 +24,13 @@ public class Hotel {
        // this.regularRate = regularRate;
         this.weekdayRate = weekdayRate;
         this.weekendRate = weekendRate;
+    }
+
+    public Hotel(String name, int weekdayRate, int weekendRate, int rating) {
+        this.name = name;
+        this.weekdayRate = weekdayRate;
+        this.weekendRate = weekendRate;
+        this.rating = rating;
     }
 
     public double getRegularRate() {
@@ -52,20 +61,28 @@ public class Hotel {
         return weekendRate;
     }
 
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
     public void setWeekendRate(double weekendRate) {
         this.weekendRate = weekendRate;
     }
 
     public static void main(String[] args) {
 
-        Hotel lakewood = new Hotel("lakewood" , 110 ,90);
-        Hotel bridgewood = new Hotel("bridgewood" , 150 ,50);
-        Hotel ridgewood = new Hotel("ridgewood" , 220 ,150);
+        Hotel lakewood = new Hotel("lakewood" , 110 ,90,3);
+        Hotel bridgewood = new Hotel("bridgewood" , 150 ,50, 4);
+        Hotel ridgewood = new Hotel("ridgewood" , 220 ,150, 5);
 
-        System.out.println("Hotel Name\tWeekday Rate \t Weekend Rate");
-        System.out.println(lakewood.name + "\t " +lakewood.weekdayRate + "\t\t" +lakewood.weekendRate);
-        System.out.println(bridgewood.name + "\t " +bridgewood.weekdayRate + "\t\t" +bridgewood.weekendRate);
-        System.out.println(ridgewood.name + "\t " +ridgewood.weekdayRate + "\t\t" +ridgewood.weekendRate);
+        System.out.println("Hotel Name\tWeekday Rate \t Weekend Rate \t rating");
+        System.out.println(lakewood.name + "\t " +lakewood.weekdayRate + "\t\t" +lakewood.weekendRate + "\t\t" +lakewood.rating);
+        System.out.println(bridgewood.name + "\t " +bridgewood.weekdayRate + "\t\t" +bridgewood.weekendRate + "\t\t" +bridgewood.rating);
+        System.out.println(ridgewood.name + "\t " +ridgewood.weekdayRate + "\t\t" +ridgewood.weekendRate +"\t\t"+ridgewood.rating);
     }
 }
 
